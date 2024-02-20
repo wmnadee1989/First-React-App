78b9656faf255d6753de28a2a81234e81ea67645
wmnadee1989 <nadeesha@pmgs.lk>
2024-02-20 20:09:16 +0530

    test7

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> test nadeesha 2024 442.
+          Edit <code>src/App.js</code> test nadeesha 2024 4442.
         </p>
         <a
           className="App-link"
