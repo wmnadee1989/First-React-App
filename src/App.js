bd8fff0ee4e9485744dff94f77e36901e3da30a3
wmnadee1989 <nadeesha@pmgs.lk>
2024-02-21 15:18:17 +0530

    test

--- src/App.js
+++ src/App.js
@@ -5,6 +5,16 @@ function App() {
   return (
     <div className="App">
       <header className="App-header">
+      <h1> Welcome To Nadeesha Test Priject</h1>
+        <div>
+          <div></div>
+          <div>
+          <p>Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, 
+            and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.</p> 
+          </div>
+          <div></div>
+        </div>
+       
         <img src={logo} className="App-logo" alt="logo" />
         <p>
           Edit <code>src/App.js</code> test nadeesha 2024 nadee20.
